5c8c77ffe5538c41037d66b18fb62e79c00d9889
ohtorii <ikeuchi.h9@gmail.com>
2020-05-09 15:14:03 +0900

    Update dllmain.cpp

--- project/hm_splitpath/dllmain.cpp
+++ project/hm_splitpath/dllmain.cpp
@@ -1,11 +1,12 @@
 ﻿// dllmain.cpp : DLL アプリケーションのエントリ ポイントを定義します。
 #include "pch.h"
 
-BOOL APIENTRY DllMain( HMODULE hModule,
+BOOL APIENTRY DllMain( HMODULE /*hModule*/,
                        DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
+                       LPVOID /*lpReserved*/
                      )
 {
+    
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
